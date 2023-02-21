81cb5cf16ea52afc5178a77dcf4cfd9d0788f695
Ognjen_Vujicic <ognjen_vujicic@epam.com>
2023-02-21 10:52:14 +0100

    add rate limiting

--- src/main/java/com/example/cryptorecommendation/CryptoRecommendationApplication.java
+++ src/main/java/com/example/cryptorecommendation/CryptoRecommendationApplication.java
@@ -3,9 +3,11 @@ package com.example.cryptorecommendation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableCaching
 public class CryptoRecommendationApplication {
 
 	public static void main(String[] args) {
